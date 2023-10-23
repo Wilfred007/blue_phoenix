4d9c4ae0ade6b02e0f75eb961baf9fca928404dd
Wilfred Adzer <adzerwilfred007@gmail.com>
2023-10-23 19:45:09 +0100

    landing page

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+function Navbar() {
+  return (
+    <nav class="relative mx-auto p-6 shadow-md">
+        <div class="flex items-center justify-between">
+            <div class="pt-2">
+                <Image src="/BlueLogo.jpg" alt="" class="" height={50} width={50} />
+            </div>
+            <div class="hidden md:flex space-x-6">
+                <a href="#" class="hover:text-gray-400 text-sm">About</a>
+                <a href="#" class="hover:text-gray-400 text-sm">Services</a>
+                <a href="#" class="hover:text-gray-400 text-sm">Our Team</a>
+                <a href="#" class="hover:text-gray-400 text-sm">Community</a>
+            </div>
+
+           <Link href='#' className='hidden md:block px-6 bg-blue-900 p-1.5 rounded-md text-white'>Career</Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  
+    
+      theme: {
+        screens: {
+          sm: "480px",
+          md: "768px",
+          lg: "976px",
+          xl: "1440px",
+        },
+        extend: {
+          colors: {
+            blueBlack: "#1a1b4b",
+            buttons: "#3a9efd",
+          },
+        },
       },
-    },
-  },
+     
+  
+
   plugins: [],
-}
+};
